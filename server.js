5f838fac16783108f1edf9aa6159fa219f5975c1
IlliaMaliarenko76712 <illyamalyarenko540@gmail.com>
2026-06-06 14:05:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://admin:admin123@cluster0-shard-00-00.aldypx7.mongodb.net:27017,cluster0-shard-00-01.aldypx7.mongodb.net:27017,cluster0-shard-00-02.aldypx7.mongodb.net:27017/lubimy-czytac?ssl=true&replicaSet=atlas-aldypx7-shard-0&authSource=admin&retryWrites=true&w=majority';
-app.use(express.static(path.join(__dirname, 'public')));
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://admin:admin123@cluster0.aldypx7.mongodb.net/?appName=Cluster0';
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log('Połączono z bazą danych'))
-  .catch(err => console.error('Błąd bazy:', err));
+  .catch(err => console.error('Błąd połączenia z bazą:', err));
 
 const bookSchema = new mongoose.Schema({
   title: String,
@@ -19,23 +18,30 @@ const bookSchema = new mongoose.Schema({
 
 const Book = mongoose.model('Book', bookSchema);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/api/books', async (req, res) => {
   try {
     const books = await Book.find();
     res.json(books);
-  } catch (error) {
-    res.status(500).json({ error: 'Błąd pobierania' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 });
 
 app.post('/api/books', async (req, res) => {
+  const book = new Book({
+    title: req.body.title,
+    author: req.body.author,
+    description: req.body.description
+  });
   try {
-    const { title, author, description } = req.body;
-    const newBook = new Book({ title, author, description });
-    await newBook.save();
+    const newBook = await book.save();
     res.status(201).json(newBook);
-  } catch (error) {
-    res.status(500).json({ error: 'Błąd zapisu' });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
